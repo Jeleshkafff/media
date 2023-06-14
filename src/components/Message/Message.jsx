188944452c3a847cebe78fb7c0a6f38bed63e0a5
うきこ <87617334+Jeleshkafff@users.noreply.github.com>
2023-06-14 11:22:39 +0300

    mess

--- src/components/Message/Message.jsx
+++ src/components/Message/Message.jsx
@@ -10,9 +10,32 @@ import Header from "../Header";
 
 export default function Message() {
   return (
-    <div>
+    <div className={styles.mesages}>
       <Header />
       {/* <h1 className={styles.news}>Новости</h1> */}
+      <div className={styles.peoples}>
+        <div className={styles.prokrutka}>
+          {/* {" "} */}
+          <ul>
+            <li>
+              {" "}
+              <img src="" alt="" /> <h2 className={styles.Name}>dss</h2>
+            </li>
+            <li>
+              {" "}
+              <img src="" alt="" /> <h2 className={styles.Name}>dss</h2>
+            </li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+        </div>
+      </div>
       {/* <ComponentsUser /> */}
       {/* <Posts /> */}
       {/* <Post/> */}
